b9c9c001165404bdda9046dec36dc4d36724c7d8
Jeff Linahan <jeff.linahan@gmail.com>
2020-04-02 23:26:20 -0400

    diagonals now drawn in different color

--- draw_polygon.py
+++ draw_polygon.py
@@ -23,12 +23,27 @@ fileh = open(sys.argv[1], 'r')
 lines = fileh.readlines()
 
 points = []
+count = 0
+has_diag = False
+diagonals = []
 
 for line in lines:
     coords = line.split()
+    if "diagonals" in coords:
+        has_diag = True
+        break
     x = int(coords[0][0:-1])
     y = int(coords[1])
     points.append((x, y))
+    count = count + 1
+
+if has_diag:
+    diagonals = []
+    for line in lines[count+1:]:
+        indices = line.split()
+        a = int(indices[0][0:-1])
+        b = int(indices[1])
+        diagonals.append((a, b))
 
 xscale = yscale = 10
 xoffset = yoffset = 10
@@ -53,6 +68,13 @@ while running:
 
         pygame.draw.line(screen, (0, 255, 0), (x1, y1), (x2, y2), 5)
         lastpoint = point
+
+    for diagonal in diagonals:
+        x1 = points[diagonal[0]][0]*xscale+xoffset
+        y1 = points[diagonal[0]][1]*xscale+xoffset
+        x2 = points[diagonal[1]][0]*xscale+xoffset
+        y2 = points[diagonal[1]][1]*xscale+xoffset
+        pygame.draw.line(screen, (255, 0, 0), (x1, y1), (x2, y2), 5)
 
     pygame.display.flip()
 
