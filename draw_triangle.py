da1296d543d60511ec71825fddc32a46f4802de1
Jeff Linahan <jeff.linahan@gmail.com>
2020-04-02 22:18:34 -0400

    draw_triangle.py can now read list of points from file and render them

--- draw_triangle.py
+++ draw_triangle.py
@@ -4,28 +4,47 @@
 import pygame
 pygame.init()
 
-# Set up the drawing window
-screen = pygame.display.set_mode([500, 500])
+screen = pygame.display.set_mode([1000, 1000])
 
-# Run until the user asks to quit
+fileh = open('polys/square', 'r')
+lines = fileh.readlines()
+
+count = 0
+points = []
+
+for line in lines:
+    coords = line.split()
+    x = int(coords[0][0:-1])
+    y = int(coords[1])
+    print(' line:' + str(x) + '; ' + str(y))
+    points.append((x, y))
+
+print('points: ' + str(points))
+
+xscale = yscale = 10
+xoffset = yoffset = 10
+
 running = True
 while running:
 
-    # Did the user click the window close button?
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # Fill the background with white
-    screen.fill((255, 255, 255))
+    screen.fill((0, 0, 255))
 
+    #pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+    lastpoint = points[-1]
+    for point in points:
 
-    # Draw a solid blue circle in the center
-    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
-    pygame.draw.line(screen, (0, 0, 0), (0, 0), (450, 450), 5)
+        x1 = point[0]*xscale+xoffset
+        y1 = point[1]*yscale+yoffset
+        x2 = lastpoint[0]*xscale+xoffset
+        y2 = lastpoint[1]*yscale+yoffset
 
+        pygame.draw.line(screen, (0, 255, 0), (x1, y1), (x2, y2), 5)
+        lastpoint = point
 
-    # Flip the display
     pygame.display.flip()
 
 
